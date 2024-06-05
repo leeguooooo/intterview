17d9e691f8d625363dc74c2ce48d522154646140
guoli <guoli@zhihu.com>
2024-06-05 18:03:10 +0800

    feat: pwaa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,35 @@ const config = defineUserConfig({
   "lang": "zh-CN",
   "title": "前端面试题集锦",
   "description": "为前端开发者准备的面试题库",
+  "head": [
+    ["link", { "rel": "manifest", "href": "/manifest.json" }],
+    ["meta", { "name": "theme-color", "content": "#3eaf7c" }],
+    ["meta", { "name": "apple-mobile-web-app-capable", "content": "yes" }],
+    [
+      "meta",
+      { "name": "apple-mobile-web-app-status-bar-style", "content": "black" }
+    ],
+    [
+      "link",
+      { "rel": "apple-touch-icon", "href": "/images/icons/icon-152x152.png" }
+    ],
+    [
+      "link",
+      {
+        "rel": "mask-icon",
+        "href": "/images/icons/safari-pinned-tab.svg",
+        "color": "#3eaf7c"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "msapplication-TileImage",
+        "content": "/images/icons/icon-144x144.png"
+      }
+    ],
+    ["meta", { "name": "msapplication-TileColor", "content": "#000000" }]
+  ],
   "theme": defaultTheme({
     "logo": "images/logo.webp",
     "hostname": "https://interview.leeguoo.com",
