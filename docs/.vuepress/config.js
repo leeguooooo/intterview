1b063234115c550e4115af70f48c576150d6b531
guoli <guoli@zhihu.com>
2024-06-06 14:25:58 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -368,22 +368,22 @@ const config = defineUserConfig({
           }
         ]
       },
-      {
-        "text": "文章",
-        "link": "/article/"
-      },
-      {
-        "text": "分类",
-        "link": "/category/"
-      },
-      {
-        "text": "标签",
-        "link": "/tag/"
-      },
-      {
-        "text": "时间线",
-        "link": "/timeline/"
-      },
+      // {
+      //   "text": "文章",
+      //   "link": "/article/"
+      // },
+      // {
+      //   "text": "分类",
+      //   "link": "/category/"
+      // },
+      // {
+      //   "text": "标签",
+      //   "link": "/tag/"
+      // },
+      // {
+      //   "text": "时间线",
+      //   "link": "/timeline/"
+      // },
       {
         "text": "React",
         "children": [
